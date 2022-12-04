96b1dc9184dedda3cc6491ec489121aa69ef59cd
arghpy <subaandrei2@gmail.com>
2022-12-04 11:38:29 +0200

    modified shown fields in dwmblocks

--- .local/src/dwmblocks/blocks.def.h
+++ .local/src/dwmblocks/blocks.def.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "disk_usage",		30,		16},
+	/*{"", "disk_usage",		30,		16},*/
 	{"", "memory",			5,		10},
-	{"", "cpu_stats",		30,		15},
-	{"", "cpu_temp",		5,		12},
+	/*{"", "cpu_stats",		30,		15},*/
+	/*{"", "cpu_temp",		5,		12},*/
 	{"", "battery",			30,		11},
 	{"", "volume",			0,		13},
 	{"", "wifi",			10,		17},
